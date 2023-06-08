f24a3808586e1213d3fa8b8cd43db8e457f6f2a2
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 07:14:10 +0200

    Update MMM-next-episode.js

--- MMM-next-episode.js
+++ MMM-next-episode.js
@@ -17,24 +17,27 @@ Module.register('MMM-next-episode', {
     },
 
     processData: function(data) {
-        return data.map((showData) => {
-            const splitData = showData.split(' ');
-            const icon = splitData[4].split('/')[2];
-            const showName = icon.split('.')[0];
-            const airDate = splitData[6];
-            return {
-                name: showName,
-                icon: icon,
-                airDate: airDate
-            };
-        });
+        let dataArr = data.trim().split(' ');
+        let processedData = [];
+        for(let i=0; i<dataArr.length; i+=7){
+            processedData.push({
+                id: dataArr[i],
+                time: dataArr[i+1],
+                season: dataArr[i+2],
+                episode: dataArr[i+3],
+                icon: dataArr[i+4].split('/')[2],
+                showName: dataArr[i+4].split('/')[2].split('.')[0],
+                airDate: dataArr[i+5] + ' ' + (dataArr[i+6] || ''),
+            });
+        }
+        return processedData;
     },
 
     getDom: function() {
         var wrapper = document.createElement('div');
         this.shows.forEach((show) => {
             var showElement = document.createElement('div');
-            showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.name} - ${show.airDate}`;
+            showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.showName} - ${show.airDate}`;
             wrapper.appendChild(showElement);
         });
         return wrapper;
