31f88ed0f04528c8a3744a4a21de489c0bad41d8
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 08:39:18 +0200

    Update MMM-next-episode.js

--- MMM-next-episode.js
+++ MMM-next-episode.js
@@ -18,29 +18,30 @@ Module.register('MMM-next-episode', {
         }
     },
 
-processData: function(data) {
-    console.log("Processing data: ", data);
-    let dataArr = data.trim().split('\n');
-    let processedData = [];
-    for(let i=0; i<dataArr.length; i++){
-        let showDataArr = dataArr[i].trim().split(' ');
-        const showData = {
-            id: showDataArr[0],
-            time: showDataArr[1],
-            season: showDataArr[2],
-            episode: showDataArr[3],
-            icon: showDataArr[4].split('/')[4].split('?')[0],
-            showName: showDataArr[4].split('/')[4].split('?')[0].split('.')[0],
-            airDate: showDataArr.slice(5).join(' '),
-        };
-        console.log("Processed show data: ", showData);
-        processedData.push(showData);
-    }
-    return processedData;
-},
-
+    processData: function(data) {
+        console.log("Processing data: ", data);
+        let dataArr = data.trim().split('\n');
+        let processedData = [];
+        for(let i=0; i<dataArr.length; i++){
+            let showDataArr = dataArr[i].trim().split(' ');
+            const showData = {
+                id: showDataArr[0],
+                time: showDataArr[1],
+                season: showDataArr[2],
+                episode: showDataArr[3],
+                icon: showDataArr[4].split('/')[4].split('?')[0],
+                showName: showDataArr[4].split('/')[4].split('?')[0].split('.')[0],
+                airDate: showDataArr.slice(5).join(' '),
+            };
+            console.log("Processed show data: ", showData);
+            processedData.push(showData);
+        }
+        console.log("Final processed data: ", processedData);
+        return processedData;
+    },
 
     getDom: function() {
+        console.log("Creating DOM elements");
         var wrapper = document.createElement('div');
         this.shows.forEach((show) => {
             console.log("Creating DOM element for show: ", show.showName);
