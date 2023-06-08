2fbc82cb920a35558b7b2d7d39c8987ba7406a16
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 09:38:56 +0200

    Update MMM-next-episode.js

--- MMM-next-episode.js
+++ MMM-next-episode.js
@@ -5,22 +5,22 @@ Module.register('MMM-next-episode', {
     },
 
     start: function() {
-        console.log("MMM-next-episode started!");
+        console.log("MMM-next-episode started in main module!");
         this.shows = [];
         this.sendSocketNotification('GET_DATA', this.config);
     },
 
     socketNotificationReceived: function(notification, payload) {
-        console.log("next-episode, Received socket notification: ", notification, " with payload: ", payload);
+        console.log("next-episode, Received socket notification in main module: ", notification, " with payload: ", payload);
         if (notification === 'DATA') {
-            console.log("next-episode, next-episode, Received DATA notification with payload: ", payload);
+            console.log("next-episode, Received DATA notification with payload in main module: ", payload);
             this.shows = this.processData(payload);
             this.updateDom();
         }
     },
 
     processData: function(data) {
-        console.log("next-episode, Processing data: ", data);
+        console.log("next-episode, Processing data in main module: ", data);
         let dataArr = data.trim().split('\n');
         let processedData = [];
         for(let i=0; i<dataArr.length; i++){
@@ -35,25 +35,25 @@ Module.register('MMM-next-episode', {
                     showName: showDataArr[4].split('/')[4].split('?')[0].split('.')[0],
                     airDate: showDataArr.slice(5).join(' '),
                 };
-                console.log("next-episode, Processed show data: ", showData);
+                console.log("next-episode, Processed show data in main module: ", showData);
                 processedData.push(showData);
             } catch (error) {
-                console.error("next-episode, Error occurred when processing data: ", error);
+                console.error("next-episode, Error occurred when processing data in main module: ", error);
             }
         }
-        console.log("next-episode, Final processed data: ", processedData);
+        console.log("next-episode, Final processed data in main module: ", processedData);
         return processedData;
     },
 
     getDom: function() {
-        console.log("next-episode, Creating DOM elements");
+        console.log("next-episode, Creating DOM elements in main module");
         var wrapper = document.createElement('div');
-this.shows.forEach((show) => {
-    console.log("next-episode, Creating DOM element for show: ", show.showName, " with air date: ", show.airDate);
-    var showElement = document.createElement('div');
-    showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.showName} - ${show.airDate}`;
-    wrapper.appendChild(showElement);
-});
+        this.shows.forEach((show) => {
+            console.log("next-episode, Creating DOM element for show: ", show.showName, " with air date: ", show.airDate);
+            var showElement = document.createElement('div');
+            showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.showName} - ${show.airDate}`;
+            wrapper.appendChild(showElement);
+        });
         return wrapper;
     }
 });
