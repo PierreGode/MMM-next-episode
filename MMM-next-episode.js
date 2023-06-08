f86fa846b62de629d2f538861e8e70f06a93d19b
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 07:15:34 +0200

    Update MMM-next-episode.js

--- MMM-next-episode.js
+++ MMM-next-episode.js
@@ -5,22 +5,25 @@ Module.register('MMM-next-episode', {
     },
 
     start: function() {
+        console.log("MMM-next-episode started!");
         this.shows = [];
         this.sendSocketNotification('GET_DATA', this.config);
     },
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'DATA') {
+            console.log("Received DATA notification with payload: ", payload);
             this.shows = this.processData(payload);
             this.updateDom();
         }
     },
 
     processData: function(data) {
+        console.log("Processing data: ", data);
         let dataArr = data.trim().split(' ');
         let processedData = [];
         for(let i=0; i<dataArr.length; i+=7){
-            processedData.push({
+            const showData = {
                 id: dataArr[i],
                 time: dataArr[i+1],
                 season: dataArr[i+2],
@@ -28,7 +31,9 @@ Module.register('MMM-next-episode', {
                 icon: dataArr[i+4].split('/')[2],
                 showName: dataArr[i+4].split('/')[2].split('.')[0],
                 airDate: dataArr[i+5] + ' ' + (dataArr[i+6] || ''),
-            });
+            };
+            console.log("Processed show data: ", showData);
+            processedData.push(showData);
         }
         return processedData;
     },
@@ -36,6 +41,7 @@ Module.register('MMM-next-episode', {
     getDom: function() {
         var wrapper = document.createElement('div');
         this.shows.forEach((show) => {
+            console.log("Creating DOM element for show: ", show.showName);
             var showElement = document.createElement('div');
             showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.showName} - ${show.airDate}`;
             wrapper.appendChild(showElement);
