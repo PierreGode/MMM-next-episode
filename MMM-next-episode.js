6ba1868d5d9aff50aef0509fea50dac596830dcc
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 06:50:02 +0200

    Create MMM-next-episode.js

---
+++ MMM-next-episode.js
@@ -0,0 +1,37 @@
+Module.register('MMM-next-episode', {
+    start: function() {
+        this.shows = [];
+        this.sendSocketNotification('GET_DATA');
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'DATA') {
+            this.shows = this.processData(payload);
+            this.updateDom();
+        }
+    },
+
+    processData: function(data) {
+        return data.map((showData) => {
+            const splitData = showData.split(' ');
+            const icon = splitData[4].split('/')[2];
+            const showName = icon.split('.')[0];
+            const airDate = splitData[6];
+            return {
+                name: showName,
+                icon: icon,
+                airDate: airDate
+            };
+        });
+    },
+
+    getDom: function() {
+        var wrapper = document.createElement('div');
+        this.shows.forEach((show) => {
+            var showElement = document.createElement('div');
+            showElement.innerHTML = `<img src="https://static.next-episode.net/tv-shows-images/thumb/${show.icon}" /> ${show.name} - ${show.airDate}`;
+            wrapper.appendChild(showElement);
+        });
+        return wrapper;
+    }
+});
