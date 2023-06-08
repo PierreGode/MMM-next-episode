4a5c240abfbf1868aec17605c468989700529795
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 09:30:02 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 module.exports = NodeHelper.create({
     start: function() {
-        console.log("Node Helper started!");
+        console.log("next-episode, Node Helper started!");
     },
 
     socketNotificationReceived: function(notification, payload) {
@@ -16,7 +16,7 @@ module.exports = NodeHelper.create({
     getData: function(config) {
         var self = this;
         var url = `https://next-episode.net/api/magicmirror/v1/services.php?service=next&user_id=${config.id}&hash_key=${config.hash_key}`;
-        console.log("Requesting data from: ", url);
+        console.log("next-episode, Requesting data from: ", url);
         request({ url: url, method: 'GET' }, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log("next-episode, Successfully received data from the API");
