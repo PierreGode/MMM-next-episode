b5e4307a9254d92b52a5d7ed36bb7df62adc9445
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 08:53:27 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -2,12 +2,9 @@ var NodeHelper = require('node_helper');
 var request = require('request');
 
 module.exports = NodeHelper.create({
-    start: function() {
-        console.log("Node Helper started!");
-    },
+    start: function() {},
 
     socketNotificationReceived: function(notification, payload) {
-        console.log("Received socket notification: ", notification, " with payload: ", payload);
         if (notification === 'GET_DATA') {
             this.getData(payload);
         }
@@ -16,15 +13,17 @@ module.exports = NodeHelper.create({
     getData: function(config) {
         var self = this;
         var url = `https://next-episode.net/api/magicmirror/v1/services.php?service=next&user_id=${config.id}&hash_key=${config.hash_key}`;
-        console.log("Requesting data from: ", url);
         request({ url: url, method: 'GET' }, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("next-episode, Successfully received data from the API");
-                self.sendSocketNotification('DATA', body);
-            } else {
-                console.error("next-episode, Error in getData: ", error);
-                console.log("next-episode, Response status code: ", response && response.statusCode);
+            if (error) {
+                console.error("next-episode, Error occurred in HTTP request: ", error);
+                return;
             }
+            if (response.statusCode != 200) {
+                console.error("next-episode, Received non-200 status code: ", response.statusCode);
+                return;
+            }
+            console.log("next-episode, Successfully received data from the API: ", body);
+            self.sendSocketNotification('DATA', body);
         });
     }
 });
