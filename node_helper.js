a30ef4a51dae4a80c7635d593d4590c5f11442ae
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-08 14:22:43 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,29 +1,49 @@
 var NodeHelper = require('node_helper');
 var request = require('request');
-var qrcode = require('qrcode');  // add this line to import the qrcode library
-var uuidv4 = require('uuid').v4; // add this line to import the uuid library
+var qrcode = require('qrcode');  // Import the qrcode library
+var uuidv4 = require('uuid').v4; // Import the uuid library
 
 module.exports = NodeHelper.create({
     start: function() {
         console.log("next-episode, Node Helper started!");
+        this.timer = null; // Declare a variable to hold the interval timer
+    },
+
+    stop: function() {
+        // Clear the interval when the module is stopped
+        if (this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
     },
 
     socketNotificationReceived: function(notification, payload) {
         console.log("next-episode, Received socket notification: ", notification, " with payload: ", payload);
         if (notification === 'GET_DATA') {
             this.getData(payload);
+
+            // Clear existing timer if any
+            if (this.timer !== null) {
+                clearInterval(this.timer);
+            }
+
+            // Set interval to fetch data every 24 hours
+            var self = this;
+            this.timer = setInterval(function() {
+                self.getData(payload);
+            }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
         }
     },
 
     getData: function(config) {
         var self = this;
-        // check if id and hash_key are empty
+        // Check if id and hash_key are empty
         if(config.id === '' && config.hash_key === '') {
-            // generate unique device ID
+            // Generate unique device ID
             var deviceId = uuidv4();
-            // create url for QR code
+            // Create URL for QR code
             var url = `https://next-episode.net/api/magicmirror/v1/services.php?service=link&device_id=${deviceId}&username=USERNAME&password=PASSWORD`;
-            // generate QR code
+            // Generate QR code
             qrcode.toDataURL(url, function (err, url) {
                 if (err) {
                     console.log("next-episode, Error generating QR Code: ", err);
