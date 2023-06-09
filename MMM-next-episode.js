ba022d7da00eb5b09dcd147bfe202ef6d9fa816a
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-06-09 09:23:12 +0200

    Update MMM-next-episode.js

--- MMM-next-episode.js
+++ MMM-next-episode.js
@@ -2,13 +2,15 @@ Module.register('MMM-next-episode', {
     defaults: {
         id: '',
         hash_key: '',
-        displaySeasonAndEpisode: false
+        displaySeasonAndEpisode: false,
+        ShowThumbnail: true, //added to enable thumbnail functionality
+        ThumbnailSize: 'little' //added to set thumbnail size
     },
 
     start: function() {
         console.log("MMM-next-episode started!");
         this.shows = [];
-        this.qrCode = null; // added to hold QR code
+        this.qrCode = null;
         this.sendSocketNotification('GET_DATA', this.config);
     },
 
@@ -18,7 +20,7 @@ Module.register('MMM-next-episode', {
             console.log("next-episode, Received DATA notification with payload: ", payload);
             this.shows = this.processData(payload);
             this.updateDom();
-        } else if (notification === 'QR_CODE') {  // handle QR_CODE notification
+        } else if (notification === 'QR_CODE') { 
             console.log("next-episode, Received QR_CODE notification with payload: ", payload);
             this.qrCode = payload;
             this.updateDom();
@@ -49,7 +51,8 @@ Module.register('MMM-next-episode', {
                     season: result.getElementsByTagName('seasonNumber')[0].textContent,
                     episode: result.getElementsByTagName('episodeNumber')[0].textContent,
                     showName: showName,
-                    airDate: result.getElementsByTagName('countdown')[0].textContent
+                    airDate: result.getElementsByTagName('countdown')[0].textContent,
+                    thumbnail: `https://static.next-episode.net/tv-shows-images/${this.config.ThumbnailSize}/${showName}.jpg`, // thumbnail URL added to show data
                 };
                 console.log("next-episode, Processed show data: ", showData);
                 processedData.push(showData);
@@ -78,11 +81,18 @@ Module.register('MMM-next-episode', {
                 if (isNaN(airDateDays) || airDateDays <= this.config.maxdays) {
                     console.log("next-episode, Creating DOM element for show: ", show.showName, " with season and episode: S", show.season, "E", show.episode, " and air date: ", show.airDate);
                     var showElement = document.createElement('div');
+
+                    if (this.config.ShowThumbnail) { // If ShowThumbnail is true, create img element with thumbnail URL
+                        var img = document.createElement('img');
+                        img.src = show.thumbnail;
+                        showElement.appendChild(img);
+                    }
+
                     var capitalizedShowName = show.showName.charAt(0).toUpperCase() + show.showName.slice(1);
                     if (this.config.displaySeasonAndEpisode) {
-                        showElement.innerHTML = `${capitalizedShowName}: S${show.season}E${show.episode} ${show.airDate}`;
+                        showElement.innerHTML += `${capitalizedShowName}: S${show.season}E${show.episode} ${show.airDate}`;
                     } else {
-                        showElement.innerHTML = `${capitalizedShowName}: ${show.airDate}`;
+                        showElement.innerHTML += `${capitalizedShowName}: ${show.airDate}`;
                     }
                     wrapper.appendChild(showElement);
                 }
